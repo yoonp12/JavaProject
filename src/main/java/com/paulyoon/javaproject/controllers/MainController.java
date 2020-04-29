5c70f6703241f7c0598be1b2db901813eb0f007d
KevinLieuNinja <klieu925@gmail.com>
2020-04-29 15:44:49 -0700

    kevBranch

--- src/main/java/com/paulyoon/javaproject/controllers/MainController.java
+++ src/main/java/com/paulyoon/javaproject/controllers/MainController.java
@@ -34,7 +34,7 @@ public class MainController {
 	//  CREATE POST PAGE
 	@GetMapping("/new")
 	public String newPost() {
-		return "profile.jsp";
+		return "/PostImg/PostPage.jsp";
 	}
 	//  CREATE POST METHOD
 	@PostMapping(value="/newPost")
